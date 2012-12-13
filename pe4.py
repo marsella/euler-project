1f955642f5427c74027e55ee7418e969fcf0a429
Marcella <marcellahastings@gmail.com>
2012-12-13 17:54:28 -0500

    better palindrome finder

--- pe4.py
+++ pe4.py
@@ -10,30 +10,23 @@ def products():
   while i > 99:
     j = 999
     while j > 99:
-      if ispalindrome(i * j) and i * j > max:
+      if ispalv2(i * j) and i * j > max:
         max = i * j
       j -= 1
     i -= 1
   print max
 
-# this only works for 5 and 6 digit numbers. 
-# there must be a way to generalize this.
-def ispalindrome(k):
-  if k < 10000 or k > 998001:
-    return False
-  if k < 100000: #5-digits
-    if k / 10000 != k % 10:
-      return False
-    k = (k % 10000 - k % 10) / 10
-    if k / 100 != k % 10:
-      return False
-  else:
-    if k / 100000 != k % 10:
-      return False
-    k = (k % 100000 - k % 10) / 10
-    if k / 1000 != k % 10:
-      return False
-    k = (k % 1000 - k % 10) / 10
-    if k / 10 != k % 10:
-      return False
+# cleaner palindrome verifier
+def ispalv2(k):
+  dig = len(str(k))  
+  if True: # dig % 2 != 0:
+    dig -= 1
+    while(dig >= 1):
+      if k / pow(10, dig) != k % 10:
+        return False
+      k = (k % pow(10, dig) - k % 10) / 10
+      dig -= 2
   return True
+
+
+
