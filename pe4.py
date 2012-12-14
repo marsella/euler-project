0bfb1643f3fcad4d121f1cd0d395df052217d735
Marcella <marcellahastings@gmail.com>
2012-12-14 16:13:15 -0500

    p4: cleaning up brute force

--- pe4.py
+++ pe4.py
@@ -7,25 +7,23 @@ import math
 def products():
   i = 999
   max = 0
-  while i > 99:
-    j = 999
-    while j > 99:
+# how to do sequence?
+  nums = [p+100 for p in range(0, 900)]
+  for i in nums:
+    for j in nums:
       if ispalv2(i * j) and i * j > max:
         max = i * j
-      j -= 1
-    i -= 1
   print max
 
 # cleaner palindrome verifier
 def ispalv2(k):
   dig = len(str(k))  
-  if True: # dig % 2 != 0:
-    dig -= 1
-    while(dig >= 1):
-      if k / pow(10, dig) != k % 10:
-        return False
-      k = (k % pow(10, dig) - k % 10) / 10
-      dig -= 2
+  dig -= 1
+  while(dig >= 1):
+    if k / pow(10, dig) != k % 10:
+      return False
+    k = (k % pow(10, dig) - k % 10) / 10
+    dig -= 2
   return True
 
 
