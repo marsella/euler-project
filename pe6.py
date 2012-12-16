23a1a2429e0aefd94119410f00174d5bd6d6dfaf
Marcella <marcellahastings@gmail.com>
2012-12-16 13:40:48 -0500

    problem 6 + algebra

--- pe6.py
+++ pe6.py
@@ -14,10 +14,11 @@ def sumofsq(n):
     sum += i * i
   return sum
 
-
+# haha. algebra.
 def sqofsums(n):
-  sum = 0
-  for i in range(0, n + 1):
-    sum += i
-  return sum * sum
+  s = (n + 1) * (n / 2)
+  if n % 2 != 0:
+    s += (n / 2) + 1
+  print s * s
+
 
