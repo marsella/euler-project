3a2f9e52f357a0c9daef1f8235076dba4b97b582
Marcella <marcellahastings@gmail.com>
2012-12-22 10:11:13 -0600

    organizing

--- PE1-9/pe2.py
+++ PE1-9/pe2.py
@@ -1,21 +1,10 @@
 # find sum of even-valued terms of the fibonacci sequence under 4 mil.
 
-# huh. those mods sure are ugly.
+# every third number in the fibonacci sequence is even.
 def fib(k):
-  a, b = 1, 2
-  sum = 0
-  while b < k: # b is always bigger
-    if a % 2 == 0: sum += a
-    if b % 2 == 0: sum += b
-    a = a + b
-    b = a + b
-  print sum
-
-# hold on. every third number in the fibonacci sequence is even.
-def fibv2(k):
   a = b = 1
   sum = 0
-  while(b < k):
+  while b < k:
     c = a + b
     sum += c
     a = b + c
