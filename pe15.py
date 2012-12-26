ea496ca478fc7957acf9ab088c965f17d2729ace
Marcella <marcellahastings@gmail.com>
2012-12-26 12:54:18 -0600

    15

---
+++ pe15.py
@@ -0,0 +1,14 @@
+# max number of paths in a kxk grid
+
+# can this be simplified?
+def maxpaths(k):
+  pathlen = k + k 
+  num = fact(2*k)
+  kf = fact(k)
+  print num / (kf * kf)
+
+def fact(k):
+  if k < 1:
+    return 1
+  return k * fact(k-1)
+
