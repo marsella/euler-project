d676087a0d758bec27cd5205950c31b24b004bba
Marshall <mmoutenot@gmail.com>
2012-12-30 01:13:14 -0600

    realize this was already solved and feel bad for making gratuitous stylistic changes

--- pe19.py
+++ pe19.py
@@ -1,19 +1,26 @@
 # how many firsts of the months were Sundays in the 20th century?
 # http://projecteuler.net/problem=19
 
+START_YEAR = 1901
+END_YEAR   = 2001
+
 def firsts():
-  year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  sundays = 0
-  days = -5 #1901 starts on a Tues
-  for i in range(1901, 2001):
-    if is_leap_year(i): year[1] = 29
-    else: year[1] = 28
+  days_in_year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  num_sundays = 0
+  days = -5 # 1901 starts on a Tues
+  for i in range(START_YEAR, END_YEAR):
+    # Feb gets extra day for leap year
+    if is_leap_year(i): days_in_year[1] = 29
+    else: days_in_year[1] = 28
+
     for m in range(12):
       if days % 7 == 0:
-        sundays += 1
-      days += year[m]
+        num_sundays += 1
+      days += days_in_year[m]
     days %= 7
-  print sundays
 
-def is_leap_year(year):
-  return (not year % 400) or (year % 100) or (not year % 4)
+  print num_sundays
+
+def is_leap_year(y): return (not y % 400) or (y % 100) or (not y % 4)
+
+firsts()
