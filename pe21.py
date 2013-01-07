fbd4ca59c3ceaca74073240c05c338eac6cb50fa
Marcella <marcellahastings@gmail.com>
2013-01-06 18:46:41 -0600

    megabus inspirations

--- pe21.py
+++ pe21.py
@@ -13,10 +13,12 @@ def sumam(n):
   return ct
 
 def d(n):
+  if n == 0: return 0
   ct = 1
   for j in range(2, 1 + int(sqrt(n))):
     if not n % j: 
       ct += j + n/j
+      if j == n/j: ct -= j
   return ct
 
 print sumam(10001)
