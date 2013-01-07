c41ed9d3aef92c8f8097aadcb6fa9a8087fdb70c
Marcella <marcellahastings@gmail.com>
2013-01-07 15:10:30 -0600

    woo fancy python

--- pe22.py
+++ pe22.py
@@ -2,24 +2,17 @@
 # project_euler.com/problem=22
 
 
-def alphabetize(names):
-  names.sort()
-  # replace each name with its score
-  for i in range(len(names)):
-    score = 0
-    for l in names[i]:
-      if str.isalpha(l):
-        score += ord(str.upper(l)) - ord('A') + 1
-    score *= (i + 1)
-    names[i] = score 
-  print sum(names)
-
-def main(file_name = "names.txt"):
+def alphabetize(file_name = "names.txt"):
   f = open(file_name, 'r')
   names = []
   for line in f:
-    names += string.split(line, ',')
-  alphabetize(names)
+    names = str.split(str.replace(str.replace(line, '\"', ''), '\r', ''), ',')
+  names.sort()
+  # replace each name with its score
+  alphaval = dict(zip([chr(x) for x in range(65, 91)], range(1, 27)))
+  for i in range(len(names)):
+    names[i] = sum(alphaval[l] for l in names[i]) * (i + 1)
+  print sum(names)
 
 
 
