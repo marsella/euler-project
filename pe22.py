dda3eb6d8bb36b1bc39b3288c425054eb348fd6d
Marcella <marcellahastings@gmail.com>
2013-01-07 10:01:20 -0600

    string methods 101

--- pe22.py
+++ pe22.py
@@ -1,23 +1,24 @@
 # alphebetize a list, calculate scores, sum the scores
 # project_euler.com/problem=22
 
+
 def alphabetize(names):
   names.sort()
   # replace each name with its score
   for i in range(len(names)):
     score = 0
     for l in names[i]:
-      score += string.uppercase.index(l) + 1
+      if str.isalpha(l):
+        score += ord(str.upper(l)) - ord('A') + 1
     score *= (i + 1)
     names[i] = score 
   print sum(names)
-
 
 def main(file_name = "names.txt"):
   f = open(file_name, 'r')
   names = []
   for line in f:
-    names += string.split(string.replace(line, '\"', ''), ',')
+    names += string.split(line, ',')
   alphabetize(names)
 
 
