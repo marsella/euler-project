f3e084d3ec123851e1edceecdc6d9836a7d5861f
Marcella <marcellahastings@gmail.com>
2013-01-10 13:09:14 -0600

    prettier

--- pe24.py
+++ pe24.py
@@ -2,22 +2,22 @@
 # projecteuler.net/problem=24
 
 def lexo(seq):
-  length = len(seq)
-  if length == 1:
+  if len(seq) == 1:
     return seq
   else:
     toret = []
     for i in range(len(seq)):
-      val = seq[i]
-      seq.remove(val)
-      for x in lexo(seq):
-        toret.append(val + x)
-      seq.insert(i, val)
+      for x in lexo(seq[:i]+seq[i+1:]): 
+        toret.append(seq[i] + x)
     return toret
 
+import time
+
+stime = time.time()
 seq = [str(x) for x in range(10)]
 final = lexo(seq)
 print final[1000000]
+print time.time() - stime
 
 
 
