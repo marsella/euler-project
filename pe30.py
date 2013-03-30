98df432247288b71a9274b9e18ea13a0d9d285b5
Marcella <marcellahastings@gmail.com>
2013-03-30 17:18:26 -0400

    now with upper bounds and enumerations

--- pe30.py
+++ pe30.py
@@ -1,20 +1,23 @@
 # project euler 30
 # sum of powers of digits
+# now with upper bounds and enumerations
 
 POWER = 5
-UPPER_BOUND = 900000 # how to find this?
-
 digits = [i**POWER for i in range(10)]
+
+numdigits = 1
+while numdigits * digits[9] > 10**numdigits:
+  numdigits += 1
+print numdigits
+UPPER_BOUND = int('9' * numdigits) # this is very high. but it works
 
 def sumofdigits(i):
   return sum([digits[int(n)] for n in str(i)])
   
 sums = [sumofdigits(i) for i in range(UPPER_BOUND)]
-for i in range(UPPER_BOUND):
-  if sums[i] != i:
-    sums[i] = 0
+answers = [i for i,x in enumerate(sums) if i == x]
 
-print sum(sums) - 1
+print sum(answers) - 1
 
 
 
