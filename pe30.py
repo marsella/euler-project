3ac2fa62c609538bf866cf6dfd8a2fe5e3636b59
Marcella <marcellahastings@gmail.com>
2013-03-30 17:33:21 -0400

    lowered upper bound

--- pe30.py
+++ pe30.py
@@ -5,11 +5,11 @@
 POWER = 5
 digits = [i**POWER for i in range(10)]
 
+# upper bound calculations
 numdigits = 1
 while numdigits * digits[9] > 10**numdigits:
   numdigits += 1
-print numdigits
-UPPER_BOUND = int('9' * numdigits) # this is very high. but it works
+UPPER_BOUND = digits[9] * numdigits
 
 def sumofdigits(i):
   return sum([digits[int(n)] for n in str(i)])
@@ -18,9 +18,4 @@ sums = [sumofdigits(i) for i in range(UPPER_BOUND)]
 answers = [i for i,x in enumerate(sums) if i == x]
 
 print sum(answers) - 1
-
-
-
-
-
 
